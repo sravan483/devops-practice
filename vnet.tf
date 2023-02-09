c5d2780d6c518827d7cc2757453d13e405785091
skyeruva <skyeruva@outlook.com>
2023-02-10 00:09:18 +0530

    subnet 1 2 3 added

--- vnet.tf
+++ vnet.tf
@@ -9,3 +9,30 @@ resource "azurerm_virtual_network" "vnet1" {
     environment = "Development"
   }
 }
+
+resource "azurerm_subnet" "subnet-1" {
+
+  name                 = "subnet-1"
+  resource_group_name  = azurerm_resource_group.devops-practice1.name
+  virtual_network_name = azurerm_virtual_network.vnet1.name
+  address_prefixes     = ["10.28.1.0/24"]
+
+}
+
+resource "azurerm_subnet" "subnet-2" {
+  name                 = "subnet-2"
+  resource_group_name  = azurerm_resource_group.devops-practice1.name
+  virtual_network_name = azurerm_virtual_network.vnet1.name
+  address_prefixes     = ["10.28.2.0/24"]
+
+}
+
+resource "azurerm_subnet" "subnet-3" {
+  name                 = "subnet-3"
+  resource_group_name  = azurerm_resource_group.devops-practice1.name
+  virtual_network_name = azurerm_virtual_network.vnet1.name
+  address_prefixes     = ["10.29.1.0/24"]
+
+}
+
+

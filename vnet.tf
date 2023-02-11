79494ca78eef24a1b1b757f8a0d010028431d47e
skyeruva <skyeruva@outlook.com>
2023-02-12 01:41:20 +0530

    added variables 1

--- vnet.tf
+++ vnet.tf
@@ -1,7 +1,7 @@
 resource "azurerm_virtual_network" "vnet1" {
   name                = var.vnet1_name
   location            = var.location
-  resource_group_name = var.rg_name
+  resource_group_name = azurerm_resource_group.devops-practice1.name
   address_space       = [var.vnet_cidr_block]
   dns_servers         = [var.dns1, var.dns2]
 
@@ -12,25 +12,25 @@ resource "azurerm_virtual_network" "vnet1" {
 
 resource "azurerm_subnet" "subnet-1" {
 
-  name                 = var.subunet-1
-  resource_group_name  = var.rg_name
-  virtual_network_name = var.vnet1_name
+  name                 = var.subunet1_name
+  resource_group_name  = azurerm_resource_group.devops-practice1.name
+  virtual_network_name = azurerm_virtual_network.vnet1.name
   address_prefixes     = [var.subnet1_cidr]
 
 }
 
 resource "azurerm_subnet" "subnet-2" {
-  name                 = var.subnet-2
-  resource_group_name  = var.rg_name
-  virtual_network_name = var.vnet1_name
+  name                 = var.subnet2_name
+  resource_group_name  = azurerm_resource_group.devops-practice1.name
+  virtual_network_name = azurerm_virtual_network.vnet1.name
   address_prefixes     = [var.subnet2_cidr]
 
 }
 
 resource "azurerm_subnet" "subnet-3" {
-  name                 = var.subnet-3
-  resource_group_name  = var.rg_name
-  virtual_network_name = var.vnet1_name
+  name                 = var.subnet3_name
+  resource_group_name  = azurerm_resource_group.devops-practice1.name
+  virtual_network_name = azurerm_virtual_network.vnet1.name
   address_prefixes     = [var.subnet3_cidr]
 
 }

f15c9e43f7693dd95d6713b6ffec9fc3ac7ba12b
skyeruva <skyeruva@outlook.com>
2023-02-16 16:49:00 +0530

    added testserver3

--- dns.tf
+++ dns.tf
@@ -10,3 +10,19 @@ resource "azurerm_dns_a_record" "testserver1" {
   ttl                 = 300
   records             = ["10.1.1.100"]
 }
+
+resource "azurerm_dns_a_record" "testserver2" {
+  name                = "testserver2"
+  zone_name           = azurerm_dns_zone.devopssravancom.name
+  resource_group_name = azurerm_resource_group.devops-practice1.name
+  ttl                 = 300
+  records             = ["10.1.1.101"]
+}
+
+resource "azurerm_dns_a_record" "testserver3" {
+  name                = "testserver3"
+  zone_name           = azurerm_dns_zone.devopssravancom.name
+  resource_group_name = azurerm_resource_group.devops-practice1.name
+  ttl                 = 300
+  records             = ["10.1.1.102"]
+}

e214a1a04c32070bae769176fbc7337d6e24a092
skyeruva <skyeruva@outlook.com>
2023-02-16 16:45:54 +0530

    added testserver1

--- dns.tf
+++ dns.tf
@@ -2,3 +2,11 @@ resource "azurerm_dns_zone" "devopssravancom" {
   name                = "devopssravan.com"
   resource_group_name = azurerm_resource_group.devops-practice1.name
 }
+
+resource "azurerm_dns_a_record" "testserver1" {
+  name                = "testserver1"
+  zone_name           = azurerm_dns_zone.devopssravancom.name
+  resource_group_name = azurerm_resource_group.devops-practice1.name
+  ttl                 = 300
+  records             = ["10.1.1.100"]
+}
